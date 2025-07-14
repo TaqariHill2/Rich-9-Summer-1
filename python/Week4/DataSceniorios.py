443db09433af5fcb867ba92c03eb45af53bdb0ce
Taqari Hill <taqari.hill@hgs.hiddengeniusproject.org>
2025-07-14 10:58:56 -0700

    done

---
+++ python/Week4/DataSceniorios.py
@@ -0,0 +1,19 @@
+#1. A restaurant menu with prices for each item
+#2. High scores to an arcade game
+#3. All of the months of the year
+#4. All the items in your backpack
+#5. Look up Student emails by their names
+#6. A shopping cart of groceries 
+#7. [Add a scenario of your own]
+print("A resturant menu with prics for each item")
+print("Best structure:Dictionary: Best to pair food with prices un my key/vaule")
+menu = {
+    "French Toast": 12,
+    "Grand slam": 12,
+    "T-Bone Steak": 40,
+    "Ramen Noodles": 10
+}
+or Key item in menu. items():
+print ("Key, ": ", item")
+
+
